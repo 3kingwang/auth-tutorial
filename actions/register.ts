1e702420045436202ff804652439af276aad2462
3kingwang <103560098+3kingwang@users.noreply.github.com>
2024-01-18 09:30:59 +0800

    register user

--- actions/register.ts
+++ actions/register.ts
@@ -1,12 +1,24 @@
 "use server"
 
+import { db } from "@/lib/db"
+import bcrypt from "bcryptjs"
 import { RegisterSchema } from "@/schemas"
 import * as z from "zod"
+import { getUserByEmail } from "@/data/user"
 
-export const register = async(values: z.infer<typeof RegisterSchema>) => {
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedfields = RegisterSchema.safeParse(values)
   if (!validatedfields.success) {
     return { error: "Invalid fields!" }
   }
-  return { success: "Email sent" }
+  const { name, email, password } = validatedfields.data
+  const hashedPassword = await bcrypt.hash(password, 10)
+  const existingUser = await getUserByEmail(email)
+  if (existingUser) {
+    return { error: "Email already registered!" }
+  }
+  await db.user.create({ data: { name, email, password: hashedPassword } })
+  // TODO: Send confirmation email
+
+  return { success: "User created successfully!" }
 }
