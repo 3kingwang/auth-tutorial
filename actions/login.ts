414f19bc697c6da57ae702de69384fb2b98c11fc
3kingwang <103560098+3kingwang@users.noreply.github.com>
2024-01-18 14:41:09 +0800

    finish login by credentials

--- actions/login.ts
+++ actions/login.ts
@@ -2,11 +2,31 @@
 
 import { LoginSchema } from "@/schemas"
 import * as z from "zod"
+import { signIn } from "@/auth"
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes"
+import { AuthError } from "next-auth"
 
-export const login = async(values: z.infer<typeof LoginSchema>) => {
+export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedfields = LoginSchema.safeParse(values)
   if (!validatedfields.success) {
     return { error: "Invalid fields!" }
   }
-  return { success: "Email sent" }
+  const { email, password } = validatedfields.data
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    })
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid email or password!" }
+        default:
+          return { error: "Something went wrong!" }
+      }
+    }
+    throw error
+  }
 }
